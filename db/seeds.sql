564e8c7ba567628b45def3b0134cb5fdae27b97c
Kristina Pang <kvypang@gmail.com>
2022-04-29 13:53:26 -0700

    schema and seeds

---
+++ db/seeds.sql
@@ -0,0 +1,15 @@
+INSERT INTO movies (movie_name)
+VALUES 
+("Godzilla"),
+("Superman"),
+("Batman"),
+("Harry Potter"),
+("Sonic"),
+
+INSERT INTO reviews (movie_id, review)
+VALUES 
+(1, "Action packed!"),
+(2, "Superman saves the day!"),
+(3, "Amazing!"),
+(4, "Well written!"),
+(5, "Great family movie."),
--- server.js
+++ server.js
@@ -6,3 +6,15 @@ const app = express();
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+const db = mysql.createConnection(
+    {
+      host: 'localhost',
+      user: 'root',
+      password: 'password',
+      database: 'movies_db'
+    },
+    console.log(`Connected to the movies_db database.`)
+  );
+
+  
